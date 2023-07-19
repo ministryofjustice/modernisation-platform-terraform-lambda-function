7cb2ef9f4422cbc064ef160663ab435ba52f9695
Bogdan Mania <bogdan.mania@digital.justice.gov.uk>
2023-07-19 09:43:54 +0100

    add invoke_arn lambda function output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "lambda_function_arn" {
   value       = try(aws_lambda_function.this.arn, "")
 }
 
+output "lambda_function_invoke_arn" {
+  description = "The invoke ARN of the Lambda Function"
+  value       = try(aws_lambda_function.this.invoke_arn, "")
+}
+
 output "lambda_function_name" {
   description = "The Name of the Lambda Function"
   value       = try(aws_lambda_function.this.function_name, "")
