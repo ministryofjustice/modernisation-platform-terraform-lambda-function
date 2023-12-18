f6d15f813bbd9596d40c5a2374428ef20ddcb167
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-12-18 17:18:29 +0000

    updating outputs

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -11,9 +11,9 @@ output "result_code" {
 }
 
 output "security_group_id" {
-  value = module.module_test.lambda_function_security_group_id
+  value = module.module_lambda_vpc_test.vpc_security_group_ids
 }
 
 output "subnet_id" {
-  value = module.module_test.lambda_function_subnet_id
+  value = module.module_lambda_vpc_test.vpc_subnet_ids
 }
