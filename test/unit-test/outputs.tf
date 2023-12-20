179c70875bf053ebdfaf2627c5a4a137c38cf7ec
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-12-20 11:00:08 +0000

    go tests to check for subnet and sg ids created from new lambda test module

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -17,7 +17,3 @@ output "security_group_ids" {
 output "subnet_ids" {
   value = module.lambda_function_in_vpc.vpc_subnet_ids
 }
-
-# output "result_code_vpc" {
-#   value = jsondecode(aws_lambda_invocation.test_invocation_vpc.result)["statusCode"]
-# }
