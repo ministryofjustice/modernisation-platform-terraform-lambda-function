76f1447e7ca01dc74486fdaed9c773203b2334f9
mikereiddigital <mike.reid@justice.gov.uk>
2026-06-17 12:52:45 +0100

    Updated s3 module ref to v10.1.0. Also added dependent kms resource & policy.

--- test/unit-test/s3.tf
+++ test/unit-test/s3.tf
@@ -1,6 +1,8 @@
 module "s3-bucket" {
-  source             = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399" # v9.0.0
-  bucket_prefix      = "data-platform-products-${local.environment}"
+  source             = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=76321e50b20f5c0d918cd45bdcf0b62049f5baf1" # v10.1.0
+  bucket_prefix      = random_id.s3_bucket_prefix.hex
+  sse_algorithm      = "aws:kms"
+  custom_kms_key     = aws_kms_key.s3.arn
   versioning_enabled = false
   # Refer to the below section "Replication" before enabling replication
   replication_enabled = false
@@ -55,3 +57,62 @@ module "s3-bucket" {
 
   tags = local.tags
 }
+
+data "aws_iam_policy_document" "s3_bucket_kms" {
+  statement {
+    sid    = "AllowAccountAdministration"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowS3UseOfTheKey"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+      "kms:ReEncrypt*",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["s3.${data.aws_region.current_region.name}.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:s3:arn"
+      values   = ["arn:aws:s3:::${random_id.s3_bucket_prefix.hex}*"]
+    }
+  }
+}
+
+resource "random_id" "s3_bucket_prefix" {
+  byte_length = 4
+}
+
+resource "aws_kms_key" "s3" {
+  description             = "KMS key for use by S3 in Lambda unit test"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.s3_bucket_kms.json
+}
+
